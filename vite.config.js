b26a2ebcbc77e86488fdf868907dbf68e3725704
leejiajunat2026 <leejiajunat2026@gmail.com>
2026-08-08 16:02:06 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/MyTakken/'  // 将 './' 修改为 '/仓库名/'
+})
 export default defineConfig({ plugins: [vue()], base: './' })
